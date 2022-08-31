343efba5c1cff3cf5dc8a46adeb89e302ae3c00d
Avishai Brown <avishai.brown@gmail.com>
2022-08-31 11:02:05 -0400

    No search results

--- src/containers/SearchResults/SearchResults.js
+++ src/containers/SearchResults/SearchResults.js
@@ -2,29 +2,49 @@ import React from "react";
 import MovieCard from "../../components/MovieCard/MovieCard";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
+import Container from "@mui/material/Container";
+import {
+  NO_RESULTS_FIRST_LINE,
+  NO_RESULTS_SECOND_LINE,
+} from "../../utils/constants";
 
 const SearchResults = (props) => {
   const { setMovieId, movies } = props;
 
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <Grid
-        container
-        spacing={{ xs: 2, md: 3 }}
-        columns={{ xs: 4, sm: 8, md: 12 }}
-        justifyContent="flex-start"
-        alignItems="flex-start"
-      >
-        {movies?.map((movie) => (
-          <Grid item xs={2} sm={2} md={2} key={movie.imdbID}>
-            <MovieCard
-              key={movie.imdbID}
-              movie={movie}
-              setMovieId={setMovieId}
-            />
+      {movies ? (
+        <Grid
+          container
+          spacing={{ xs: 2, md: 3 }}
+          columns={{ xs: 4, sm: 8, md: 12 }}
+          justifyContent="flex-start"
+          alignItems="flex-start"
+        >
+          {movies?.map((movie) => (
+            <Grid item xs={2} sm={2} md={2} key={movie.imdbID}>
+              <MovieCard
+                key={movie.imdbID}
+                movie={movie}
+                setMovieId={setMovieId}
+              />
+            </Grid>
+          ))}
+        </Grid>
+      ) : (
+        <Container maxWidth={"sm"}>
+          <Grid justifyContent="center" zeroMinWidth p={5}>
+            <p>
+              <img
+                src="./images/illustrations-empty-state.png"
+                alt="empty state"
+              ></img>
+            </p>
+            <p>{NO_RESULTS_FIRST_LINE}</p>
+            <p>{NO_RESULTS_SECOND_LINE}</p>
           </Grid>
-        ))}
-      </Grid>
+        </Container>
+      )}
     </Box>
   );
 };
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,6 +4,8 @@ export const BACK_TO_SEARCH = "Back To Search";
 export const API_KEY = "dbb0b270";
 
 export const SEARCH_BAR_PLACEHOLDER = "Type to search for a movie";
+export const NO_RESULTS_FIRST_LINE = "Don't know what to search?";
+export const NO_RESULTS_SECOND_LINE = "Here's an offer you can't refuse";
 
 export const IMDB = "Internet Movie Database";
 export const ROTTEN_TOMATOES = "Rotten Tomatoes";
