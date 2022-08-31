642a46332c10650eb978cf706e88257be55ea02b
Avishai Brown <avishai.brown@gmail.com>
2022-08-31 14:20:36 -0400

    Details page styling

--- src/containers/MovieDetails/MovieDetails.js
+++ src/containers/MovieDetails/MovieDetails.js
@@ -1,6 +1,11 @@
 import React, { useEffect, useRef } from "react";
 import { NavLink } from "react-router-dom";
-import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import Card from "@mui/material/Card";
+import CardMedia from "@mui/material/CardMedia";
+import { CardActionArea } from "@mui/material";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import {
   ADD_TO_FAVOURITES,
   IMDB,
@@ -9,14 +14,22 @@ import {
   CAST,
   GENRE,
   DIRECTOR,
-  BACK_TO_SEARCH,
 } from "../../utils/constants";
 
 const MovieDetails = (props) => {
   const { movieDetails } = props;
-  let imdbRating,
-    rottenTomatoesRating = null;
+
+  //TODO: Get all of these variables displayed
+  let imdbRating = null;
+  let rottenTomatoesRating = null;
+  let actorsArray = [];
+  let directorsArray = [];
+  let genresArray = [];
+
   const getRatings = useRef(() => {});
+  const getActors = useRef(() => {});
+  const getDirectors = useRef(() => {});
+  const getGenres = useRef(() => {});
 
   getRatings.current = () => {
     for (const index in movieDetails?.Ratings) {
@@ -28,37 +41,81 @@ const MovieDetails = (props) => {
     }
   };
 
+  getActors.current = () => {
+    actorsArray = movieDetails?.Actors.split(", ");
+  };
+
+  getDirectors.current = () => {
+    directorsArray = movieDetails?.Director.split(", ");
+  };
+
+  getDirectors.current = () => {
+    genresArray = movieDetails?.Genre.split(", ");
+  };
+
   useEffect(() => {
-    movieDetails !== {} && getRatings.current();
+    movieDetails?.Ratings && getRatings.current();
+    movieDetails?.Actors && getActors.current();
+    movieDetails?.Director && getDirectors.current();
+    movieDetails?.Genre && getGenres.current();
   }, [movieDetails]);
 
   return (
-    <>
-      <NavLink to="/search" style={{ textDecoration: "none" }}>
-        <Button variant="contained">{BACK_TO_SEARCH}</Button>
-      </NavLink>
-      <p>{movieDetails?.Runtime}</p>
-      <p>{movieDetails?.Year}</p>
-      <p>{movieDetails?.Rated}</p>
-      <p>{movieDetails?.Title}</p>
-      <p>{imdbRating}</p>
-      {rottenTomatoesRating && <p>{rottenTomatoesRating}</p>}
-      <button>{ADD_TO_FAVOURITES}</button>
-      <p>{PLOT}</p>
-      <p>{movieDetails?.Plot}</p>
-      <p>{CAST}</p>
-      {/* {Object.values(movieDetails?.Actors).map((member) => (
-        <li>{member}</li>
-      ))} */}
-      <p>{GENRE}</p>
-      {/* {movieDetails?.Genre.map((genre) => (
-        <li>{genre}</li>
-      ))} */}
-      <p>{DIRECTOR}</p>
-      {/* {movieDetails?.Director.map((name) => (
-        <li>{name}</li>
-      ))} */}
-    </>
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container>
+        <Grid item xs={6}>
+          <NavLink to="/search" style={{ textDecoration: "none" }}>
+            <ArrowBackIcon />
+          </NavLink>
+          <Grid item xs={6} className="infoAboveTitle">
+            <p>{movieDetails?.Runtime}</p>
+            <p>{movieDetails?.Year}</p>
+            <p>{movieDetails?.Rated}</p>
+          </Grid>
+          <h1>{movieDetails?.Title}</h1>
+          <p>{imdbRating}</p>
+          {rottenTomatoesRating && <p>{rottenTomatoesRating}</p>}
+          <button>{ADD_TO_FAVOURITES}</button>
+          <p>{PLOT}</p>
+          <p>{movieDetails?.Plot}</p>
+          {actorsArray && (
+            <>
+              <p>{CAST}</p>
+              {actorsArray.map((member) => (
+                <li>{member}</li>
+              ))}
+            </>
+          )}
+          {genresArray && (
+            <>
+              <p>{GENRE}</p>
+              {genresArray.map((member) => (
+                <li>{member}</li>
+              ))}
+            </>
+          )}
+          {directorsArray && (
+            <>
+              <p>{DIRECTOR}</p>
+              {directorsArray.map((member) => (
+                <li>{member}</li>
+              ))}
+            </>
+          )}
+        </Grid>
+        <Grid item xs={6}>
+          <Card key={movieDetails?.imdbID}>
+            <CardActionArea>
+              <CardMedia
+                component="img"
+                image={movieDetails?.Poster}
+                alt="movie"
+              />
+            </CardActionArea>
+          </Card>
+        </Grid>
+      </Grid>
+    </Box>
   );
 };
 
